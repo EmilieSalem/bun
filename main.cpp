f5dc05fbd0afb9a113e0df8a90369ab1b01bb54c
EmilieSalem <70089578+EmilieSalem@users.noreply.github.com>
2023-03-15 10:32:04 +0100

    created the prototype window and bunny shape

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    sf::RenderWindow fenetre{sf::VideoMode{800, 600}, "bun"};
+    sf::RectangleShape bunny(sf::Vector2f(50, 50));
+    bunny.setFillColor(sf::Color(100,59,53));
+
+    while (fenetre.isOpen()) {
+        auto evenement = sf::Event();
+
+        while (fenetre.pollEvent(evenement)) {
+            if (evenement.type == sf::Event::Closed) fenetre.close();
+        }
+
+        fenetre.clear(sf::Color{230, 202, 188});
+        fenetre.draw(bunny);
+        fenetre.display();
+    }
 }
