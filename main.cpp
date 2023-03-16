d51519aa64e865170e0e3dd71c3f7fb0bc54ef81
EmilieSalem <70089578+EmilieSalem@users.noreply.github.com>
2023-03-16 14:51:15 +0100

    implemented basic gaming loop code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    sf::RenderWindow window{sf::VideoMode(), "bun", sf::Style::Fullscreen};
+    while(window.isOpen()){
+        auto event = sf::Event();
+
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
 }
