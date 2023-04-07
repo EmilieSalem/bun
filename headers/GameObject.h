10c16ff27c90e0ecf45e3f23e807798d1722dfcb
EmilieSalem <70089578+EmilieSalem@users.noreply.github.com>
2023-04-07 18:13:18 +0200

    cleaned and wrote documentation for the GameObject class

--- headers/GameObject.h
+++ headers/GameObject.h
@@ -4,64 +4,118 @@
 #include <SFML/Graphics.hpp>
 #include "../headers/Utils.h"
 
+/// Defines the different types of GameObjects.
 enum class ObjectType{BUNNY, PLATFORM, CARROT, OTHER};
 
+/// An element of the game, represented by a sprite.
+/// The GameObject class only defines overarching methods, specific behaviors are defined in its subclasses.
 class GameObject {
 public:
-    // constructor
-    explicit GameObject(
-            std::string_view const& assetPath,
-            sf::Vector2f const& initialPosition,
-            sf::Vector2f const& p_scale = sf::Vector2f(1, 1)
-                    );
 
-    // managing position
-    inline float getX() const {return position.x;};
-    inline float getY() const {return position.y;};
-    inline float getWidth() const {return sprite.getGlobalBounds().width;};
-    inline float getHeight() const {return sprite.getGlobalBounds().height;};
-    void update(float loopTime);
-    void display(sf::RenderWindow& window) const;
+    // CONSTRUCTOR | DESTRUCTOR --------------------------------------------------------------------
+    /// Initializes a GameObject.
+    /// \param assetPath Path to the image file used as a texture for the sprite.
+    /// \param initialPosition Initial position vector.
+    /// \param p_scale Scale of the sprite. Defaults to (1,1).
+    explicit GameObject(std::string_view const& assetPath,
+                        sf::Vector2f const& initialPosition,
+                        sf::Vector2f const& p_scale = sf::Vector2f(1, 1));
 
-    // remove an object from the game
-    void remove();
-    inline bool isRemoved() const {return removed;};
-
-    // managing collisions
-    inline ObjectType getType() const {return type;};
-    virtual void testCollision(GameObject& otherObject);
-    virtual void handleCollision(GameObject& otherObject);
-    virtual void manageScreenLimits();
-
-    // disabling copy
+    /// Disables the possibility to construct a GameObject from another.
+    /// \param other Other GameObject.
     GameObject(GameObject const& other) = delete;
+
+    /// Disables the possibility to copy a GameObject.
+    /// \param other Other GameObject.
     void operator=(GameObject const& other) = delete;
 
-    // creating virtual destructor
+    /// Makes the destructor virtual.
     virtual ~GameObject() = default;
 
+    // GETTERS --------------------------------------------------------------------
+    /// Gets the x position of the sprite.
+    /// \return x position of the sprite.
+    inline float getX() const {return position.x;};
+
+    /// Gets the y position of the sprite.
+    /// \return y position of the sprite.
+    inline float getY() const {return position.y;};
+
+    /// Gets the width of the sprite.
+    /// \return Width of the sprite
+    inline float getWidth() const {return sprite.getGlobalBounds().width;};
+
+    /// Gets the height of the sprite.
+    /// \return Height of the sprite.
+    inline float getHeight() const {return sprite.getGlobalBounds().height;};
+
+    /// Indicates whether the GameObject is currently removed from the screen.
+    /// \return Boolean indicating if the GameObject is removed.
+    inline bool isRemoved() const {return removed;};
+
+    /// Gets the specific type of the GameObject.
+    /// \return Type of the GameObject. Can be BUNNY, PLATFORM, CARROT or OTHER.
+    inline ObjectType getType() const {return type;};
+
+    // UPDATE | DISPLAY --------------------------------------------------------------------
+
+    /// Updates the position and velocity of the GameObject, as well as its related objects (e.g. a charge bar).
+    /// Specific behavior of the object (gravity, user input...) and screen limits are enforced.
+    /// \param loopTime Duration of the game loop in seconds.
+
+    void update(float loopTime);
+
+    /// Removes a GameObject from the screen.
+    void remove();
+
+    /// Displays the GameObject and its related objects in the given window.
+    /// \param window SFML window in which the game runs.
+    void display(sf::RenderWindow& window) const;
+
+    // COLLISIONS --------------------------------------------------------------------
+
+    /// Tests if a collision is happening with another GameObject.
+    /// If it is the case, handles the collision.
+    /// \param otherObject Other GameObject involved in the collision.
+    virtual void testCollision(GameObject& otherObject);
+
+    /// Applies the right collision behavior to the GameObject (changes in position, velocity...).
+    /// \param otherObject Other GameObject involved in the collision.
+    virtual void handleCollision(GameObject& otherObject);
+
+    /// Applies the right behavior to the GameObject when it goes outside the screen limits.
+    virtual void manageScreenLimits();
+
 protected:
-    // appearance
+    /// Sprite of the GameObject.
     sf::Sprite sprite{};
+
+    /// Texture applied to the sprite.
     sf::Texture texture{};
+
+    /// Scale of the sprite.
     sf::Vector2f scale{};
 
-    // physics
+    /// Type of the GameObject, defaults to OTHER.
     ObjectType type{ObjectType::OTHER};
+
+    /// Position vector.
     sf::Vector2f position{};
+
+    /// Velocity vector.
     sf::Vector2f velocity{};
     virtual void applyBehavior(float loopTime);
 
-    // remove
+    /// Indicates if the GameObject is currently removed from the screen.
     bool removed{false};
+
+    /// Defines additional instructions to perform when removing an object from the screen.
     virtual void onRemoval();
 
-    // debug
-    inline void setRed(){sprite.setColor(sf::Color::Red);};
-    inline void setGreen(){sprite.setColor(sf::Color::Green);};
-
-    // to display elements related to the GameObject, such as a ChargeBar
+    /// Updates the state of objects that directly depend on the GameObject (e.g. a charge bar).
     virtual void updateRelatedObjects();
+
+    /// Displays objects related to the GameObject.
     virtual void displayRelatedObjects(sf::RenderWindow& window) const;
 };
 
--- sources/GameObject.cpp
+++ sources/GameObject.cpp
@@ -2,60 +2,59 @@
 #include <cmath>
 #include <iostream>
 
-GameObject::GameObject(
-        const std::string_view &assetPath,
-        const sf::Vector2f &initialPosition,
-        const sf::Vector2f &p_scale
-        ) : position{initialPosition}, scale{p_scale}{
-    // loading and applying texture
+// CONSTRUCTOR --------------------------------------------------------------------
+
+GameObject::GameObject(const std::string_view &assetPath,
+                       const sf::Vector2f &initialPosition,
+                       const sf::Vector2f &p_scale)
+                       : position{initialPosition}, scale{p_scale}{
     texture.loadFromFile(assetPath.data());
     sprite.setTexture(texture);
     sprite.setScale(scale);
-
-    // setting origin and position
     sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
     sprite.setPosition(position.x, position.y);
 }
 
+// UPDATE | DISPLAY --------------------------------------------------------------------
+
 void GameObject::update(const float loopTime) {
-    applyBehavior(loopTime);
+    applyBehavior(loopTime); // applying object-specific behavior (gravity, user input...)
     position += velocity * loopTime;
-    manageScreenLimits();
+    manageScreenLimits(); // enforcing screen limits may change the position
     sprite.setPosition(position.x, position.y);
     updateRelatedObjects();
 }
 
 void GameObject::display(sf::RenderWindow &window) const {
     // TODO Reimplement duplication
-    /*auto directions = std::array<sf::Vector2f, 9>{
-            sf::Vector2f{-1, 0},
-            sf::Vector2f{0, 0},
-            sf::Vector2f{1, 0},
-    };
-    for(auto& direction : directions){
-        auto transformation = sf::Transform{};
-        transformation.translate(
-                direction.x * Utils::getScreenWidth(),
-                direction.y * Utils::getScreenHeight()
-                );
-        window.draw(sprite, transformation);
-    }*/
     window.draw(sprite);
     displayRelatedObjects(window);
 }
 
-void GameObject::applyBehavior(const float loopTime) {}
+void GameObject::remove() {
+    scale = sf::Vector2f(0,0);
+    sprite.setScale(scale);
+    removed = true;
+    onRemoval();
+}
+
+// COLLISIONS --------------------------------------------------------------------
 
 void GameObject::testCollision(GameObject &otherObject) {
-    if(!otherObject.isRemoved()){
+    if(!otherObject.isRemoved()){ // e.g. to avoid colliding with removed carrots
         auto x_distance = static_cast<float>(std::abs(position.x - otherObject.getX()));
         auto y_distance = static_cast<float>(std::abs(position.y - otherObject.getY()));
 
+        // box collision with a bit of tolerance (dividing by 2.4 instead of 2)
         if(x_distance < getWidth()/2.4 + otherObject.getWidth()/2.4 && y_distance < getHeight()/2.4 + otherObject.getHeight()/2.4){
             handleCollision(otherObject);
         }
     }
 }
+
+// VIRTUAL METHODS --------------------------------------------------------------------
+
+void GameObject::applyBehavior(const float loopTime) {}
 
 void GameObject::manageScreenLimits() {}
 
@@ -66,10 +65,3 @@ void GameObject::displayRelatedObjects(sf::RenderWindow &window) const {}
 void GameObject::handleCollision(GameObject &otherObject) {}
 
 void GameObject::onRemoval() {}
-
-void GameObject::remove() {
-    scale = sf::Vector2f(0,0);
-    sprite.setScale(scale);
-    removed = true;
-    onRemoval();
-}
