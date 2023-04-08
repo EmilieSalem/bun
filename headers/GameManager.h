eb82fe70625fb67d197bb8b9416ff89366b17784
EmilieSalem <70089578+EmilieSalem@users.noreply.github.com>
2023-04-08 18:34:57 +0200

    cleaned and wrote documentation for the GameManager class

--- headers/GameManager.h
+++ headers/GameManager.h
@@ -4,29 +4,50 @@
 #include "ObjectManager.h"
 #include "UIManager.h"
 
+/// The GameManager is responsible for switching between the different menus of the game and triggering the appropriate game loops.
+///  It uses both an ObjectManager and a UIManager.
 class GameManager {
 public:
+    // CONSTRUCTOR --------------------------------------------------------------------
+    /// Initializes a GameManager. Default constructor, no additional behavior.
     GameManager() = default;
 
-    // manage game states
+    // GAME STATES --------------------------------------------------------------------
+    /// Defines the different game states, aka the types of screens/menus accessible to the player.
     enum class GameStates{PLAYING, START, GAME_OVER, GAME_WON};
+    /// Gets the current game state.
+    /// \return Current game state. Can be PLAYING, START, GAME_OVER or GAME_WON.
     inline GameStates currentGameState(){return gameState;};
 
-    // run game
+    // LOOPS --------------------------------------------------------------------
+    /// Initializes a new game and changes the game state to PLAYING.
     void startGame();
+    /// Runs a game loop, aka updates, handles collisions and displays the GameObjects in the current screen.
+    /// Also updates and displays the UI.
+    /// \param window SFML window in which the game runs.
     void runGameLoop(sf::RenderWindow& window);
-    void runContinueLoop(sf::RenderWindow& window);
+    /// Runs a UI loop that displays the title screen.
+    /// \param window SFML window in which the game runs.
     void runStartLoop(sf::RenderWindow& window);
-
-    // manage continue choice
+    /// Runs a UI loop that displays either a game over or a winning message,
+    /// with the option to continue playing or not.
+    /// \param window SFML window in which the game runs.
+    void runContinueLoop(sf::RenderWindow& window);
+    /// Checks for user input in the continue loop, to either restart a game or go back to the title screen.
     void processChoice();
 
 private:
+    // GAME STATES --------------------------------------------------------------------
+    /// Current state of the game. Initialized at START, aka on the title screen.
     GameStates gameState{GameStates::START};
-    ObjectManager objectManager{};
-    UIManager uiManager{};
-
+    /// Manages screens and changes the game state accordingly if the player has won (GAME_WON) or lost (GAME_LOST).
     void updateGameStateAndScreen();
+
+    // MANAGERS --------------------------------------------------------------------
+    /// GameManager used to run game loops.
+    ObjectManager objectManager{};
+    /// UIManager used to display the UI.
+    UIManager uiManager{};
 };
 
 
--- sources/GameManager.cpp
+++ sources/GameManager.cpp
@@ -1,27 +1,27 @@
 #include "../headers/GameManager.h"
 
+// LOOPS --------------------------------------------------------------------
+
 void GameManager::startGame() {
     gameState = GameStates::PLAYING;
     objectManager.initializeGame();
 }
 
 void GameManager::runGameLoop(sf::RenderWindow &window) {
+    // Updates and collisions
     objectManager.updateCurrentScreen();
     objectManager.handleCollisionsInCurrentScreen();
+
+    // UI
     uiManager.updateScoreDisplay(objectManager.getScore());
-
     uiManager.displayPLaying(window, objectManager.noMoreCarrotsInCurrentScreen());
+
+    // Display
     objectManager.displayCurrentScreen(window);
-
     window.display();
 
+    // Screen management
     updateGameStateAndScreen();
-}
-
-void GameManager::runContinueLoop(sf::RenderWindow& window) {
-    if(gameState == GameStates::GAME_OVER) uiManager.displayContinueMenu(window, true);
-    if(gameState == GameStates::GAME_WON) uiManager.displayContinueMenu(window, false);
-    window.display();
 }
 
 void GameManager::runStartLoop(sf::RenderWindow &window) {
@@ -29,22 +29,31 @@ void GameManager::runStartLoop(sf::RenderWindow &window) {
     window.display();
 }
 
+void GameManager::runContinueLoop(sf::RenderWindow& window) {
+    // displays the continue option with the right message
+    if(gameState == GameStates::GAME_OVER) uiManager.displayContinueMenu(window, true);
+    if(gameState == GameStates::GAME_WON) uiManager.displayContinueMenu(window, false);
+    window.display();
+
+}
+void GameManager::processChoice() {
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::E)){
+        if(uiManager.getChoice()){ // yes option | new game starts
+            startGame();
+        } else{ // no option | back to title screen
+            gameState = GameStates::START;
+        }
+    }
+}
+
+// GAME STATES --------------------------------------------------------------------
+
 void GameManager::updateGameStateAndScreen() {
-    objectManager.manageScreens();
+    objectManager.manageScreens(); // screen management
     if(objectManager.gameIsOver()) {
         gameState = GameStates::GAME_OVER;
     }
     if(objectManager.gameIsWon()) {
         gameState = GameStates::GAME_WON;
-    }
-}
-
-void GameManager::processChoice() {
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::E)){
-        if(uiManager.getChoice()){
-            startGame();
-        } else{
-            gameState = GameStates::START;
-        }
     }
 }
