e6451b4ba0e11726a7b47bff88514fb5c495ed3e
EmilieSalem <70089578+EmilieSalem@users.noreply.github.com>
2023-04-08 21:07:16 +0200

    commented the main function

--- main.cpp
+++ main.cpp
@@ -3,39 +3,43 @@
 #include "headers/GameManager.h"
 
  int main() {
-    // window
+
+    // SFML window in which the game runs
     sf::RenderWindow window{sf::VideoMode(), "bun", sf::Style::Fullscreen};
 
+    // for management of menus, objects and UI
     GameManager gameManager{};
 
     while(window.isOpen()){
         auto event = sf::Event();
 
         while(window.pollEvent(event)){
+            // to exit the game
             if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) window.close();
+
+            // to start the game when on the title screen
             if(event.type == sf::Event::KeyPressed && event.key.code ==  sf::Keyboard::Enter && gameManager.currentGameState() == GameManager::GameStates::START) gameManager.startGame();
         }
 
+        // clearing the window of the previous loop's display + setting the color
         window.clear(BEIGE);
+
         switch(gameManager.currentGameState()) {
 
             case GameManager::GameStates::PLAYING:
-                gameManager.runGameLoop(window);
+                gameManager.runGameLoop(window); // runs the game
                 break;
 
             case GameManager::GameStates::START:
-                gameManager.runStartLoop(window);
+                gameManager.runStartLoop(window); // title screen
                 break;
 
             case GameManager::GameStates::GAME_OVER:
-                gameManager.runContinueLoop(window);
-                gameManager.processChoice();
-                break;
-
             case GameManager::GameStates::GAME_WON:
-                gameManager.runContinueLoop(window);
+                gameManager.runContinueLoop(window); // displays the same continue menu but with a different message
                 gameManager.processChoice();
                 break;
         }
     }
+
 }
